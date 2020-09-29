b4abfddecf1e091deed8102c580aec48760e60b4
user <user@EE04>
2020-09-29 09:40:03 +0800

    修正

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("I'm ½²­§»Ê");
+		System.out.println("I'm Ian");
 		System.out.println("My nid is D0873131");
 		System.out.println("This is lab 1 hw");
 	}
